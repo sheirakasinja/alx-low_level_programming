03d83070c43d25aa3826af9a5f79664b4bd1ec4b
SheiraKasinja <kasinjasheira@yahoo.com>
2023-04-21 15:12:05 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX /	2;
-
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -25,6 +24,5 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
-
 	return (0);
 }
