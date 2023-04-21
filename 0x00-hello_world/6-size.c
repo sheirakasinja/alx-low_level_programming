ed2ee6eb7c6e62321286faa98321f7ceacecf75a
SheiraKasinja <kasinjasheira@yahoo.com>
2023-04-20 17:47:03 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+nclude <stdio>
+**
+* main - Entry point
+* Return: Always 0
+*/
+int main(void)
+{
+print("Size of a char:%ibyte(S)\n",
+sizeof(char))
+printf(size of an int: %i byte(s)\n",
+sizeof(int));
+printf(size of a long int: %i byte(s)
+sizeof(long int));
+printf("size of a long long int%ibyte(s)
+\n",sizeof(long long int));
+printf("Size of a float: %i byte(s)\n",
+sizeof(float));
+return(0);
+}
