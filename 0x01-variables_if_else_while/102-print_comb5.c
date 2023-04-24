8c6bc95b102d29b3fb7108ac95b96ae35b8a3815
SheiraKasinja <kasinjasheira@yahoo.com>
2023-04-24 02:13:29 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,7 +15,7 @@ int main(void)
 for (p = 0; p <= 98; p++)
 	{
 
-for (q = p + 1; q <= 98; q++)
+for (q = p + 1; q <= 99; q++)
 	{
 	putchar((p / 10) + '0');
 	putchar((p % 10) + '0');
@@ -28,7 +28,7 @@ for (q = p + 1; q <= 98; q++)
 	putchar(' ');
 	}
 	}
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
 
