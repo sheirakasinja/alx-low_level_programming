c65a67839a73665ba2a70fa18c5e54e09f59852c
SheiraKasinja <kasinjasheira@yahoo.com>
2023-05-15 12:50:06 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,7 +18,8 @@ char *str_concat(char *s1, char *s2)
 
 	if (s2 == NULL)
 		s2 = "";
-		i = ci = 0;
+
+	i = ci = 0;
 
 	while (s1[i] != '\0')
 		i++;
@@ -44,7 +45,6 @@ char *str_concat(char *s1, char *s2)
 		conct[i] = s2[ci];
 		i++, ci++;
 	}
-
 	conct[i] = '\0';
 	return (conct);
 }
