c88fbff4167cc8127518c45b2f5b07e2f38b6c8c
SheiraKasinja <kasinjasheira@yahoo.com>
2023-05-17 00:15:21 -0700

    libmy.a, main.h

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,18 +1,15 @@
 #include "main.h"
 /**
- * _strncat - concatenate two strings
- * using at most n bytes from src
+ * _strcat - concatenate two strings
  * @dest: input value
  * @src: input value
- * @n: input value
  *
- * Return: dest
+ * Return: void
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
-
 
 	i = 0;
 	while (dest[i] != '\0')
@@ -20,12 +17,13 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 	}
 	j = 0;
-	while (j < n && src[j] != '\0')
+	while (src[j] != '\0')
 	{
-	dest[i] = src[j];
-	i++;
-	j++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
+
 	dest[i] = '\0';
 	return (dest);
 }
