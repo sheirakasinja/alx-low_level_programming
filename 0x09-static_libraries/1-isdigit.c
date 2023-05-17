4d50187895411077a58660ab14d738b2ed0e5601
SheiraKasinja <kasinjasheira@yahoo.com>
2023-05-16 21:33:16 -0700

    libmy.a, main.h

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -3,9 +3,13 @@
 /**
  * _isdigit - checks for a digit (0 through 9)
  * @c: int to be checked
- * Return: 1 if c is a digit, 0 otherwise
+ * Return: 0 or 1
  */
+
 int _isdigit(int c)
 {
-	return (c >= '0' && c <= '9');
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
