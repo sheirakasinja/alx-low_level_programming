58d0530a1a2836122ec3e399ab4e083a85e797d2
SheiraKasinja <kasinjasheira@yahoo.com>
2023-05-24 14:34:21 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,6 +1,7 @@
 #include "function_pointers.h"
 /**
- * int_index - return index place if comparison = true, else -1
+ * int_index - return index place if comparison = true, else
+ -1
  * @array: array
  * @size: size of elements in array
  * @cmp: pointer to func of one of the 3 in main
